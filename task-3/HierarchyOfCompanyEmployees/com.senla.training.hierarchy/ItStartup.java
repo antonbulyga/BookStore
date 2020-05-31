35d0079a67f189688dcb3d01bffdba69561564e7
Anton Bulyga <exchange.program2018@gmail.com>
2020-05-31 21:04:32 +0300

    Fixed bags

--- task-3/HierarchyOfCompanyEmployees/com.senla.training.hierarchy/ItStartup.java
+++ task-3/HierarchyOfCompanyEmployees/com.senla.training.hierarchy/ItStartup.java
@@ -5,18 +5,12 @@ public class ItStartup {
     private ItSpecialist[] arrayOfItSpecialists = new ItSpecialist[0];
 
     public ItSpecialist[] create(int maxCountOfEmployee) {
-
-            AdderToArray adderToArray = new AdderToArray();
-            ItSpecialist architect = new Architect(3500);
-            arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, architect);
-            if(arrayOfItSpecialists.length <= maxCountOfEmployee) {
-                ItSpecialist javaDev = new JavaDev(3200);
-                arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, javaDev);
-            }
-            if(arrayOfItSpecialists.length <= maxCountOfEmployee) {
-            ItSpecialist javaScriptDev = new JavaScriptDev(3100);
-            arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, javaScriptDev);
-            }
+             AdderToArray adderToArray = new AdderToArray();
+                 ItSpecialist architect = new Architect(3500);
+                 arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, architect, maxCountOfEmployee);
+                 ItSpecialist javaDev = new JavaDev(3200);
+                 arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, javaDev, maxCountOfEmployee);
+                 ItSpecialist javaScriptDev = new JavaScriptDev(3100);arrayOfItSpecialists = adderToArray.add(arrayOfItSpecialists, javaScriptDev, maxCountOfEmployee);
         return arrayOfItSpecialists;
     }
 }
--- task-3/HierarchyOfCompanyEmployees/com.senla.training.hierarchy/Main.java
+++ task-3/HierarchyOfCompanyEmployees/com.senla.training.hierarchy/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         ItStartup itSturtup = new ItStartup();
-        int maxCountOfEmployee = 1;
+        int maxCountOfEmployee = 2;
         ItSpecialist[] array = itSturtup.create(maxCountOfEmployee);
         WageCounter wageCounter = new WageCounter();
         wageCounter.count(array);
